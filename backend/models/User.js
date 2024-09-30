f78581f571dbac6a483269c47d06fc6349ee50c4
Pavitra Pai <111533080+pavitrapai10@users.noreply.github.com>
2024-10-01 01:07:34 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,38 @@
 
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,  // Regular expression for email validation
+            'Please fill a valid email address'  // Custom error message if validation fails
+        ]
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        
+    },
+    password: {
+        type: String,
+        required: true
+
+    },
+    otp: {
+        type: String,
+        otpExpiry: Date,
+        isVerified: {
+            type: Boolean,
+            default: false
+        }
+    }
+
+
+});
+//
+module.exports = mongoose.model('User', userSchema);
