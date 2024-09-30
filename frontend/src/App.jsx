632c915819fc53123b9d8b85227c7cacd491c2dd
Pavitra Pai <111533080+pavitrapai10@users.noreply.github.com>
2024-10-01 01:36:31 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Signup from './pages/signup'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Signin from './pages/signin';
+import OtpVerification from './pages/otpVerification';
+import Home from './pages/home';
+import { useState } from 'react'
+import InvitePage from './pages/invitePage';
+import './App.css'
+
+function App() {
+
+
+  return (
+   <Router > 
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={<Signin />} />
+      <Route path="/otpVerification" element={<OtpVerification />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/invitepage" element={<InvitePage />} />
+    </Routes>
+   </Router>
+  )
+}
+
+export default App
