dcbf92af24c053c72d01b901d59ecb2398834d9b
Pavitra Pai <111533080+pavitrapai10@users.noreply.github.com>
2024-10-01 01:05:03 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,1 +1,89 @@
+const User = require('../models/User');
+const generateOtp = require('../utils/otpGenerator');
+const sendOtp = require('../utils/sendOtp');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async(req, res)=> {
+    const {email, phoneNumber, password} = req.body;
+     
+    try {
+        let user = await User.findOne({email});
+        if(user) return res.status(400).json({message : 'User already exists'});
+    
+        let userByPhone = await User.findOne({phoneNumber});
+        if(userByPhone) return res.status(400).json({message : 'Phone Number already exists'});
+    
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const otp = generateOtp();
+    
+        user = new User({
+            email, 
+            phoneNumber,
+            password: hashedPassword,
+            otp,
+            otpExpiry: Date.now()+ 10*60*1000
+        });
+    
+        await user.save();
+        await sendOtp(phoneNumber, otp);
+    
+        res.status(201).json({message: 'SignUp Successful'});
+    
+    } catch (error) {
+        console.error('Signup Error:', error);
+        res.status(500).json({error: error.message || 'Internal Server Error'});
+    }
+    
+
+    
+}
+
+exports.verifyOtp = async(req, res) => {
+    const {email, otp} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message: 'User not found'});
+
+        if(user.isVerified) return res.status(400).json({message: 'User already verified'});
+        if(user.otp !== otp || Date.now() > user.otpExpiry) {
+            return res.status(400).json({message: 'Invalid or Expired Token'});
+        }
+        user.isVerified = true;
+        user.otp = undefined;
+        user.otpExpiry = undefined;
+        await user.save();
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+        res.json({message: 'Login Succesful', token});
+
+    }
+    catch (error) {
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+
+
+}
+
+
+exports.signin = async(req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message : 'Invalid Credential'});
+
+        const isMatch = await bycrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({message : 'Invalid Credential'});
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+
+        res.json({message: 'Login Succesful', token});
+    }
+    catch (error) {
+        req.status(500).json({error: 'Internal Server Error'});
+
+    }
+
+};
+
 
